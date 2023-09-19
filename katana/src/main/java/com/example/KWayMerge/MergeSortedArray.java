4b3a14266dbe6e6f2586cecb8cc2b32e171f244c
TheMaos <yuanjian@hotmail.com>
2023-09-19 23:57:30 +0800

    added kwaymerge

--- katana/src/main/java/com/example/KWayMerge/MergeSortedArray.java
+++ katana/src/main/java/com/example/KWayMerge/MergeSortedArray.java
@@ -17,6 +17,29 @@ public class MergeSortedArray {
      */
 
      public MergeSortedArray(){
-
+        
      }
+
+     public static int[] mergeSorted(int[] nums1, int m, int[] nums2, int n) {
+        int p1  = m - 1; // set p1 to the last initialised element of nums1
+        int p2 = n - 1; // set p2 to the last element of nums2
+
+        int combined_length = m + n;
+        for (int p = combined_length - 1; p >= 0; p--) {
+            if (p2 < 0){
+                break;
+            }
+
+            if (p1 >= 0 && nums1[p1] > nums2[p2]){
+                nums1[p] = nums1[p1];
+                p1 -= 1;
+            } else {
+                nums1[p] = nums2[p2];
+                p2 -= 1;
+            }
+        }
+
+        return nums1;
+     }
+
 }
---
+++ katana/src/test/java/com/example/KWayMerge/MergeSortedArrayTest.java
@@ -0,0 +1,5 @@
+package com.example.KWayMerge;
+
+public class MergeSortedArrayTest {
+    
+}
