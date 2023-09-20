4a5bd2298d63677c5e49e7dd04d47133a9a2eb3f
TheMaos <yuanjian@hotmail.com>
2023-09-21 00:33:43 +0800

    added backtracking

--- katana/src/main/java/com/example/Backtracking/NQueens.java
+++ katana/src/main/java/com/example/Backtracking/NQueens.java
@@ -1,5 +1,10 @@
 package com.example.Backtracking;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class NQueens {
     /**
      * Given a chessboard of size n×n, determine how many ways n queens can be placed on the board, such that no two queens attack each other.
@@ -12,4 +17,57 @@ public class NQueens {
      * [4] If a safe position is not found, backtrack to the previous valid placement. Search for another solution.
      * [5] If a complete solution is found, add it to the results array, and backtrack to find other valid solutions in the same way.
      */
+
+     public NQueens(){
+
+     }
+     
+    public void solveNQueensRec(int n,  List<Integer> solution, int row, List<List<Integer>> results){
+        if (row == n){
+            results.add(solution);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean valid = isValidMove(row, i, solution);
+            if (valid) {
+                solution.set(row, i);
+                solveNQueensRec(n, solution, row + 1, results);
+            }
+            
+        }
+    }
+
+    public int solveNQueens(int n) {
+        List<List<Integer>> results = new ArrayList<>();
+		List<Integer> solution = new ArrayList<Integer> (Collections.nCopies(n, -1));
+        solveNQueensRec(n, solution, 0, results);
+        return results.size();
+    }
+
+    public boolean isValidMove(int proposedRow, int proposedColumn, List<Integer> solution){
+        int oldRow = 0;
+        int oldCol = 0;
+        int diagonalOffset = 0;
+
+
+        // oldCol == proposedCol --> Checks if there are any queens in the proposed column
+		// oldCol == proposedCol - diagonalOffset --> Checks if there are any queens on the bottom left diagonal to the proposed place
+		// oldCol == proposedCol + diagonalOffset --> Checks if there are any queens on the bottom right diagonal to the proposed place
+        for (int i = 0; i < proposedRow; i++) {
+            oldRow = i;
+            oldCol = solution.get(i);
+            diagonalOffset = proposedRow - oldRow;
+
+            boolean queenExistsSameColumn = oldCol == proposedColumn;
+            boolean queenExistsBottomLeft =  oldCol == proposedColumn - diagonalOffset;
+            boolean queenExistsBottomRight = oldCol == proposedColumn + diagonalOffset;
+
+            if (queenExistsSameColumn || queenExistsBottomLeft || queenExistsBottomRight){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
