ff8e7e1ff19bf281f7cd95ab82600b2a7685819a
TheMaos <yuanjian@hotmail.com>
2023-09-22 23:17:04 +0800

    added topological sort

--- katana/src/main/java/com/example/TopologicalSort/CompilationOrder.java
+++ katana/src/main/java/com/example/TopologicalSort/CompilationOrder.java
@@ -1,5 +1,14 @@
 package com.example.TopologicalSort;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+
+
 public class CompilationOrder {
     /**
      * There are a total of n classes labeled with the English alphabet (A, B, C, and so on). 
@@ -21,4 +30,60 @@ public class CompilationOrder {
      public CompilationOrder(){
 
      }
+
+     public List<Character> findCompilationOrder(ArrayList<ArrayList<Character>> dependencies) {
+        List<Character> sortedOrder = new ArrayList<>();
+
+        Map<Character, List<Character>> graph = new HashMap<>();
+        Map<Character, Integer> inDegree = new HashMap<>();
+
+        for (int i = 0; i < dependencies.size(); i++) {
+            
+            char parent = dependencies.get(i).get(1);
+            char child = dependencies.get(i).get(0);
+
+            System.out.println("Populating graph...(" + child + "," + parent + ")");
+
+            graph.computeIfAbsent(parent, k -> new ArrayList<>());
+            graph.computeIfAbsent(child, k -> new ArrayList<>());
+            
+            inDegree.computeIfAbsent(parent, k -> 0);
+            inDegree.computeIfAbsent(child, k -> 0);
+
+            graph.get(parent).add(child);
+            inDegree.put(child, inDegree.get(child) + 1);
+        }
+
+        if (graph.size() <= 0){
+            return sortedOrder;
+        }
+
+        Queue<Character> queue = new LinkedList<>();
+        for (char key : inDegree.keySet()){
+            System.out.println("Populating queue..." + key);
+            if (inDegree.get(key) == 0) {
+                queue.add(key);
+            }
+        }
+
+        while (!queue.isEmpty()){
+            char parent = queue.poll();
+            sortedOrder.add(parent);
+
+            System.out.println("Applying toplogical sort..." + parent);
+
+            List<Character> children = graph.get(parent); 
+            children.stream().forEach(child -> {
+                inDegree.put(child, inDegree.get(child) - 1);
+                if (inDegree.get(child) == 0) {
+                    queue.add(child);
+                }
+            });
+        }
+
+        if (sortedOrder.size() != graph.size()){
+            return new ArrayList<>();
+        }
+        return sortedOrder;
+    }
 }
