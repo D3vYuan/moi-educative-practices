8df49f208def1d7a5ca8778aaa5200549835f230
TheMaos <yuanjian@hotmail.com>
2023-09-24 10:07:11 +0800

    added tracking

--- katana/src/main/java/com/example/Tracking/PalindromePermutation.java
+++ katana/src/main/java/com/example/Tracking/PalindromePermutation.java
@@ -1,5 +1,8 @@
 package com.example.Tracking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PalindromePermutation {
     /**
      * For a given string, find whether or not a permutation of this string is a palindrome. 
@@ -12,9 +15,40 @@ public class PalindromePermutation {
      * [4] If the count exceeds 1, no palindromic permutation exists.
      */
 
+    Map<Character, Integer> characterCount = new HashMap<>();
+
      public PalindromePermutation(){
 
      }
 
+     public boolean permutePalindrome(String st) {
+        // If no characters appear an odd number of times, then the string is of even length and has a permutation that is a palindrome.
+        // If only one character appears an odd number of times, then the string is of odd length and has a permutation that is a palindrome.
+        // If more than one character appears an odd number of times, then the string does not have a permutation that is a palindrome.
+    
+        for (Character c : st.toCharArray()){
+            if (!characterCount.containsKey(c)){
+                characterCount.put(c, 1);
+            } else {
+                characterCount.put(c, characterCount.get(c) + 1);
+            }
+        }
+
+        int oddOccurrencesCount = 0;
+
+        for (Map.Entry<Character, Integer> entry : characterCount.entrySet()){
+            Integer value = entry.getValue();
+
+            if (value % 2 != 0){
+                oddOccurrencesCount += 1;
+            }
+        }
+
+        if (oddOccurrencesCount <= 1){
+            return true;
+        }
+
+        return false;
+    }
      
 }
