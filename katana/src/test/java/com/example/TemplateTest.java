295e3299739425d0aa613b2223887ab847facbbe
Yuan Jian Puah <pyuanjian@google.com>
2023-10-13 10:15:42 +0800

    rename main to avoid conflict

--- katana/src/test/java/com/example/TemplateTest.java
+++ katana/src/test/java/com/example/TemplateTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class MainTest {
+public class TemplateTest {
     @BeforeAll
     static void initAll() {
     }
