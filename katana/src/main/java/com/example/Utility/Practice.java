e14e5252fb56c5e6a2d2987aad2222aaf31b0819
Yuan Jian Puah <pyuanjian@google.com>
2023-10-31 13:26:45 +0800

    added practice

--- katana/src/main/java/com/example/Utility/Practice.java
+++ katana/src/main/java/com/example/Utility/Practice.java
@@ -1,7 +1,13 @@
 package com.example.Utility;
 
 public class Practice {
+
+    private static void runApplication() {
+        // Write me a function that will print hello you!
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        runApplication();
     }
 }
